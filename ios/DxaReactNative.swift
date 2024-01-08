a0a569a93c9b2f76594cf35e6eae73344dbc3fd6
David V <dvigara@medallia.com>
2024-01-08 17:12:41 +0100

    REMOVE - formatting added by mistake

--- ios/DxaReactNative.swift
+++ ios/DxaReactNative.swift
@@ -1,42 +1,42 @@
 @objc(DxaReactNative)
 class DxaReactNative: NSObject {
-    
-    @objc(initialize:withProperty:withResolver:withRejecter:)
-    func initialize(
-        account: Float,
-        property: Float,
-        resolve:RCTPromiseResolveBlock,
-        reject:RCTPromiseRejectBlock
-    ) -> Void {
-        let configuration = Configuration(
-            account: String(account),
-            property: String(property),
-            manualScreenTracking: true
-        )
-        // TODO: Delete in release
-        configuration.endpoint = .sigma
-        configuration.logLevel = .developer
-        
-        DXA.initialize(configuration)
-        resolve(true)
-    }
-    
-    @objc(startScreen:withResolver:withRejecter:)
-    func startScreen(
-        screenName: String,
-        resolve:RCTPromiseResolveBlock,
-        reject:RCTPromiseRejectBlock
-    ) -> Void {
-        DXA.startNewScreen(name: screenName)
-        resolve(true)
-    }
-    
-    @objc(stopScreen:withRejecter:)
-    func stopScreen(
-        resolve:RCTPromiseResolveBlock,
-        reject:RCTPromiseRejectBlock
-    ) -> Void {
-        DXA.stopScreen()
-        resolve(true)
-    }
+
+  @objc(initialize:withProperty:withResolver:withRejecter:)
+  func initialize(
+    account: Float, 
+    property: Float, 
+    resolve:RCTPromiseResolveBlock,
+    reject:RCTPromiseRejectBlock
+  ) -> Void {
+    let configuration = Configuration(
+      account: String(account),
+      property: String(property),
+      manualScreenTracking: true
+    )
+    // TODO: Delete in release
+    configuration.endpoint = .sigma
+    configuration.logLevel = .developer         
+
+    DXA.initialize(configuration)
+    resolve(true)
+  }
+
+  @objc(startScreen:withResolver:withRejecter:)
+  func startScreen(
+    screenName: String,
+    resolve:RCTPromiseResolveBlock,
+    reject:RCTPromiseRejectBlock
+  ) -> Void {
+    DXA.startNewScreen(name: screenName)
+    resolve(true)
+  }
+
+  @objc(stopScreen:withRejecter:)
+  func stopScreen(
+    resolve:RCTPromiseResolveBlock,
+    reject:RCTPromiseRejectBlock
+  ) -> Void {
+    DXA.stopScreen()
+    resolve(true)
+  }
 }
